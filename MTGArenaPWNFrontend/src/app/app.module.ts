3db0aaed96a22d1d540085a63c1b63d9988a3b1d
asmal <aleksander.smal@gmail.com>
2019-01-30 19:11:34 +0100

    updated angular to 7 initialize view of menubar

--- MTGArenaPWNFrontend/src/app/app.module.ts
+++ MTGArenaPWNFrontend/src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenubarComponent } from './menubar/menubar.component';
+import { LoginComponent } from './login/login.component';
+import { MainviewComponent } from './mainview/mainview.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatMenuModule} from '@angular/material/menu';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenubarComponent,
+    LoginComponent,
+    MainviewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatMenuModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
